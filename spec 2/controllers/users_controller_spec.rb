ede8bcec971c3a7f22b5eddcce5eaba26a0a2f8e
TakujiKomine <takuji.k@infratop.jp>
2023-12-27 04:56:09 +0000

    formatter追加

--- spec 2/controllers/users_controller_spec.rb
+++ spec 2/controllers/users_controller_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe UsersController, type: :controller do
         get :edit, params: { id: user.id }
       end
 
-      it 'レスポンスが成功すること' do
+      it 'レスポンスが成功すること', spec_category: "deviseの基本的な導入・認証設定" do
         expect(response).to have_http_status(:success)
       end
 
-      it '@userが正しく設定されていること' do
+      it '@userが正しく設定されていること', spec_category: "deviseの基本的な導入・認証設定" do
         expect(response.body).to include user.name
       end
     end
